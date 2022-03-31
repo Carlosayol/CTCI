0c38e16149997f6eddbfa1e2811668b327c48419
carlosayol <Carlosaolar@gmail.com>
2022-03-30 19:28:54 -0500

    add 1.1

--- Ch1/Tasks.py
+++ Ch1/Tasks.py
@@ -1,1 +1,21 @@
-print('Hola')
+#1.1
+
+test = ['asdbasdqw', 'aaaa', 'abcdefg', '']
+
+# Code here
+def check_unique_characters(word):
+  characters = []
+  for character in word:
+    if character in characters:
+      return False
+    else:
+      characters.append(character)
+  return True
+
+for word in test:
+  print(check_unique_characters(word))
+  
+
+
+
+
